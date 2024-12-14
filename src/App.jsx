3da9a96a622ad014be8b2511281bf84ac49386ff
shivam-231093 <shivam.mishra.231093@gmail.com>
2024-12-14 15:30:25 +0530

    firt

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
 
-    <div ref={containerRef} className="scroll-container justify-center">
+    <div ref={containerRef} className="scroll-container z-50 justify-center">
       <SplashScreen/>
     <Detail/>
     
